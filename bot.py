2e0ee18d26d2d470225ce0f88dd0285753981fb8
locolemat <122878505+locolemat@users.noreply.github.com>
2024-08-20 17:46:28 +0800

    More refine message

--- bot.py
+++ bot.py
@@ -8,5 +8,6 @@ token = os.getenv('BOT_TOKEN')
 commit_chat = os.getenv('COMMIT_CHAT')
 bot = TeleBot(token, parse_mode=None)
 
-def send_commit_message(msg):
-    bot.send_message(chat_id=commit_chat, text="Коммит залетел!")
+def send_commit_message(msg, author, repo):
+    message = f"Коммит залетел!\nРепозиторий: {repo}\nАвтор: {author}\nСообщение: {msg}"
+    bot.send_message(chat_id=commit_chat, text=message)
