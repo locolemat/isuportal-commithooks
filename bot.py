10b5a8e136c3925144ef719e7448b298874457f9
locolemat <122878505+locolemat@users.noreply.github.com>
2024-08-20 17:52:19 +0800

    Added commit url

--- bot.py
+++ bot.py
@@ -8,6 +8,6 @@ token = os.getenv('BOT_TOKEN')
 commit_chat = os.getenv('COMMIT_CHAT')
 bot = TeleBot(token, parse_mode=None)
 
-def send_commit_message(msg, author, repo):
-    message = f"Коммит залетел!\nРепозиторий: {repo}\nАвтор: {author}\nСообщение: {msg}"
+def send_commit_message(msg, author, repo, url):
+    message = f"Коммит залетел!\nРепозиторий: {repo}\nАвтор: {author}\nСообщение: {msg}\nСсылка: {url}"
     bot.send_message(chat_id=commit_chat, text=message)
