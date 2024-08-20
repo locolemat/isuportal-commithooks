a21ae59171f500753ad4e6d38410e301c498dc05
locolemat <122878505+locolemat@users.noreply.github.com>
2024-08-20 19:18:27 +0800

    Added parse HTML

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from telebot import TeleBot
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
 commit_chat = os.getenv('COMMIT_CHAT')
-bot = TeleBot(token, parse_mode=None)
+bot = TeleBot(token, parse_mode="HTML")
 
 def send_commit_message(msg, author, repo, url):
-    message = f"Коммит залетел!\nРепозиторий: {repo}\nАвтор: {author}\nСообщение: {msg}\nСсылка: {url}"
+    message = f"Коммит залетел!\n<b>Репозиторий</b>: {repo}\n<b>Автор</b>: {author}\n<b>Сообщение</b>: {msg}\n<b>Ссылка</b>: {url}"
     bot.send_message(chat_id=commit_chat, text=message)
