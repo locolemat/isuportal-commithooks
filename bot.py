c4d6f88f06987169b1fe8902bea04eeb7e1401f1
locolemat <122878505+locolemat@users.noreply.github.com>
2024-08-20 19:31:51 +0800

    Added emoji

--- bot.py
+++ bot.py
@@ -3,11 +3,13 @@ import os
 from dotenv import load_dotenv
 from telebot import TeleBot
 
+from emoji import emojize
+
 load_dotenv()
 token = os.getenv('BOT_TOKEN')
 commit_chat = os.getenv('COMMIT_CHAT')
 bot = TeleBot(token, parse_mode="HTML")
 
 def send_commit_message(msg, author, branch, url):
-    message = f"Коммит залетел!\n<b>Ветка</b>: {branch}\n<b>Автор</b>: {author}\n<b>Сообщение</b>: {msg}\n<b>Ссылка</b>: {url}"
+    message = emojize(f":hammer_and_pick:Коммит залетел!\n<b>Ветка</b>: {branch}\n<b>Автор</b>: {author}\n<b>Сообщение</b>: {msg}\n<b>Ссылка</b>: {url}")
     bot.send_message(chat_id=commit_chat, text=message)
