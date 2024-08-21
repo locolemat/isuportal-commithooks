0806ef3f42ac72306b0f8a04b4e080fb70bcbe84
locolemat <122878505+locolemat@users.noreply.github.com>
2024-08-21 22:39:00 +0800

    Removed redundant logs and added a welfare check endpoint

--- server.py
+++ server.py
@@ -43,10 +43,13 @@ class RequestHandler(BaseHTTPRequestHandler):
             for commit in data['commits']:
                 send_commit_message(msg=commit['message'], author=commit['author']['name'], branch=data['ref'].split('/')[-1], url=commit['url'])
         elif endpoint == '/merge':
-            with open('log_merge.txt', 'a') as f:
-                f.write(str(data))
-                f.write('\n')
+            # with open('log_merge.txt', 'a') as f:
+            #     f.write(str(data))
+            #     f.write('\n')
             send_merge_message(action=data['action'], url=data['pull_request']['html_url'])
+        elif endpoint == '/welfare_check':
+            with open('log_merge.txt', 'a') as f:
+                f.write('The service is up and running!\n')
         return
 
 if __name__ == '__main__':
